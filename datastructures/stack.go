212e6240bed5bd2b28a347e926b128797cb2c9a2
David Lanouette <David.Lanouette@Bronto.com>
2018-10-19 14:24:00 -0400

    Add the queue and stack implementations back.

---
+++ datastructures/stack.go
@@ -0,0 +1,74 @@
+package datastructures
+
+import "fmt"
+
+// Stack a last-in-first-out stack implementation
+type Stack struct {
+	head *stackElement
+	size int
+}
+
+type stackElement struct {
+	element     interface{}
+	nextElement *stackElement
+}
+
+// Should the traversal ethod actually print the contents of the stack?
+const printTraversal = false
+
+// NewStack Create a new stack instance
+func NewStack() *Stack {
+	return &Stack{}
+}
+
+// Push add a new value to the stack
+func (s *Stack) Push(val interface{}) {
+	s.traverse("--- before push")
+	element := &stackElement{val, s.head}
+	s.head = element
+	s.size++
+	s.traverse("--- after push")
+	return
+}
+
+// Pop removes the most recently added value from the stack, and removes it from the top of the stack.
+func (s *Stack) Pop() (interface{}, error) {
+	s.traverse("--- before pop")
+
+	var element = s.head
+
+	// short circuit for cases of empty stack
+	if element == nil {
+		return nil, nil
+	}
+
+	s.head = element.nextElement
+	s.size--
+
+	s.traverse("--- after pop")
+	return element.element, nil
+}
+
+// Peek returns the most recently added value from the stack, but leaves it on the stack
+func (s *Stack) Peek() interface{} {
+	return s.head.element
+}
+
+// Size returns the number of elements in the stack
+func (s *Stack) Size() int {
+	return s.size
+}
+
+// traverse A utility method to traverse the stack and print out the values.
+func (s *Stack) traverse(message string) {
+	if printTraversal {
+		fmt.Println(message)
+
+		var e = s.head
+
+		for e != nil {
+			fmt.Printf("element = %v\n", e.element)
+			e = e.nextElement
+		}
+	}
+}
---
+++ datastructures/stack_test.go
@@ -0,0 +1,163 @@
+package datastructures
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStackEmptyStack(t *testing.T) {
+	s := NewStack()
+
+	assert.Equal(t, 0, s.Size())
+
+	e, err := s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Nil(t, e)
+}
+
+func TestStackOneElement(t *testing.T) {
+	s := NewStack()
+
+	assert.Equal(t, 0, s.Size())
+
+	s.Push(1)
+	assert.Equal(t, 1, s.Size())
+
+	item, err := s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, 0, s.Size())
+	assert.Equal(t, 1, item)
+}
+
+func TestStackFewElements(t *testing.T) {
+	s := NewStack()
+
+	assert.Equal(t, 0, s.Size())
+
+	s.Push("abc")
+	s.Push("def")
+	assert.Equal(t, 2, s.Size())
+	item, err := s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, 1, s.Size())
+	assert.Equal(t, "def", item)
+
+	s.Push("ghi")
+	s.Push("jkl")
+	s.Push("mno")
+
+	assert.Equal(t, 4, s.Size())
+
+	item, err = s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, 3, s.Size())
+	assert.Equal(t, "mno", item)
+}
+
+func TestStackManyElements(t *testing.T) {
+	s := NewStack()
+
+	for i := 0; i < 1000000; i++ {
+		s.Push(i)
+	}
+
+	assert.Equal(t, 1000000, s.Size())
+
+	for i := 0; i < 1000000; i++ {
+		_, err := s.Pop()
+		assert.Nil(t, err)
+	}
+	assert.Equal(t, 0, s.Size())
+}
+
+func TestStackPeekWithOneElement(t *testing.T) {
+	s := NewStack()
+
+	s.Push("ABC")
+	assert.Equal(t, 1, s.Size())
+	e1 := s.Peek()
+	assert.Equal(t, 1, s.Size())
+	assert.Equal(t, "ABC", e1)
+
+	e2, err := s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, "ABC", e2)
+	assert.Equal(t, 0, s.Size())
+}
+
+func TestStackPeekWithFewElements(t *testing.T) {
+	s := NewStack()
+
+	s.Push("abc")
+	s.Push("def")
+	e1 := s.Peek()
+	assert.Equal(t, 2, s.Size())
+	assert.Equal(t, "def", e1)
+	assert.Equal(t, 2, s.Size())
+
+	e1, err := s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, "def", e1)
+	assert.Equal(t, 1, s.Size())
+
+	s.Push("ghi")
+	s.Push("jkl")
+	s.Push("mno")
+	assert.Equal(t, 4, s.Size())
+	e1 = s.Peek()
+	assert.Equal(t, "mno", e1)
+	assert.Equal(t, 4, s.Size())
+
+	e1, err = s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, "mno", e1)
+	assert.Equal(t, 3, s.Size())
+}
+
+func TestStackWithDifferentTypes(t *testing.T) {
+	s := NewStack()
+
+	s.Push(1)
+	s.Push("abc")
+	s.Push(3.1415)
+
+	e, err := s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, 3.1415, e)
+
+	e, err = s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, "abc", e)
+
+	e, err = s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Equal(t, 1, e)
+
+	// there should be no more elements
+	e, err = s.Pop()
+	if err != nil {
+		assert.Error(t, err)
+	}
+	assert.Nil(t, e)
+}
